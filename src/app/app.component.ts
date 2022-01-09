93b7325668f461b13b879a3e3e8a4db4634a0502
cloverhsc <clover.hsc@gmail.com>
2022-01-09 17:15:24 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
+import { retrievedBookList, addBook, removeBook } from './state/books.actions';
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { selectBookCollection, selectBooks } from './state/books.selectors';
+import { BooksService } from './book-list/books.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'myngrx13';
+  books$ = this.store.select(selectBooks);
+  bookCollection$ = this.store.select(selectBookCollection);
+
+  onAdd(bookId: string) {
+    this.store.dispatch(addBook({ bookId }));
+  }
+
+  onRemove(bookId: string) {
+    this.store.dispatch(removeBook({ bookId }));
+  }
+
+  constructor(private booksService: BooksService, private store: Store) {}
+
+  ngOnInit() {
+    this.booksService.getBooks().subscribe((books) => {
+      console.info(books);
+      this.store.dispatch(retrievedBookList({ books }));
+    });
+  }
 }
